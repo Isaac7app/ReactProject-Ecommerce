739317c1c31747ef328a46bd0e6ed2cdd89b87d0
Isaac7app <ik.appiagyei@gmail.com>
2022-02-11 14:10:13 +0100

    hompage taking shape

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Homepage from './Homepage';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello from React</h1>
-    </div>
+    <Homepage />
   );
 }
 
