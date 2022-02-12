f37f66b5c60cc48344ac20059bf37b72208a9b3e
Isaac7app <ik.appiagyei@gmail.com>
2022-02-12 10:08:53 +0100

    hompage image enlarged

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     return(
         <div style={{
           backgroundImage: `url(${imageUrl})`
-        }} className='menu-item'>
+        }}
+         className={`${size} menu-item`}
+         >
           <div className='content'>
             <div className='title'>{title}</div>
             <span className='subtitle'>SHOP NOW</span>
