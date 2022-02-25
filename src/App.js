f229723f424474babecb1a8500a75496708eaa66
Isaac7app <ik.appiagyei@gmail.com>
2022-02-25 10:09:28 +0100

    checkout toggle on checkout page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import { createStructuredSelector } from 'reselect';
 
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
 
@@ -48,6 +52,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
