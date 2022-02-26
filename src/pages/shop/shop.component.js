20bc61447d1080797b6893d94b91c66b4b025a31
Isaac7app <ik.appiagyei@gmail.com>
2022-02-26 11:38:45 +0100

    modifying the shop page into categories

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,27 +1,15 @@
 import React from "react";
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.js";
 
- class ShopPage extends React.Component {
-     constructor(props) {
-         super(props);
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component.js";
 
-         this.state = {
-             collections: SHOP_DATA,
-         };
-     }
 
-     render() {
-         const {collections} = this.state;
-         return( <div className="shop-page">
-             {
-                 collections.map(({id, ...otherCollectionProps}) => (
-                     <CollectionPreview key={id} {...otherCollectionProps} />
-                 ))
-             }
+ const ShopPage = ({ collections }) => {
+    return( 
+    <div className="shop-page">
+       <CollectionsOverview />
+    </div> 
+    )
+ };
 
-         </div> )
-     }
- }
-
+ 
  export default ShopPage;
